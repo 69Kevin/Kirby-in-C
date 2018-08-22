d98e30c6a2398573e517c5dc43fb406a8533c980
69Kevin <kevinmanhas@gmail.com>
2018-08-22 17:02:25 -0300

    22.08.2018

---
+++ Kirby/Kirby.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int main(){
+int kirby;
+
+
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmmmmmhhdmmmmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmds+/---..--...-------+oydmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMms/-................-----------/oyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdo:-..............-------------------:/sdMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMms-.................-----------------::::--:/yNMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMd+-...................----------------::::::::::/omMMMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMd+.....--.........---...----------------:::::::::::-:sNMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMNs.....:+/y.......:o++y:..-----------------::::::::::::-+hNMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMm:....-+-`-o.....-oo.  so..------------------:::::::::::::-oNMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMh-....-y- :h-.....sd`  `do..------------------::::::::::::::-/mMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMh.....-yNoym+.....+NN:-/hN:.------------------::::::::::::::::-:mMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMd......+NMMMh-....:mNMNNNMy-------------------::::::::::::::::::-/NMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMN-.....-dNNNm:.....sNNNMMNm:------------------::::::::::::::::::::-oMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMs....--/dddd/......hmmmmmm+-----------------:::::::::::::::::::::::-hMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMN-.-----/dhh+.......hddddd+-----------------::::::::::::::::::://:::-+MMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMy.--:::--os/........+dhhy:----------------::::::::::::::::::::///::::-MMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMo.-::::-------.......:/:-----:::::::::--:::::::::::::::::::///////:::-hMMMMMMMMMMM\n");
+printf("NhyyyhhhhdmmMMMMMM:-.--------:+ooo+/:-.-------:::::::::::::::::::::::::::::////////:::::hMMMMMMMMMMM\n");
+printf("soooosssyyyhhhdNMM-----------+hhyyyyy/----------::::::::::::::::::::::::////////////:::-hMMMMMMMMMMM\n");
+printf("oooooosssyyyhhhhdo::----------o+++oo+---------------::::::::::::::::::////////////:::::-yMMMMMMMMMMM\n");
+printf("sooooossssyyyhhhddy/-----------:///:--------------:::::::://+++++///////////:::::::::::::yMMMMMMMMMM\n");
+printf("ssoossssssyyyhhhdddo:--------------------------:::::::/+oooooooosssso+/////:::::::::::::::/MMMMMMMMM\n");
+printf("yssssssssssyyyhhdddh/:----------------------:::::::/+ooooooooooossssyys///:::::::::::::::::yMMMMMMMM\n");
+printf("Nsssssssssssyyyhhdddo::------------------::::::::+ooooooooooossssssssyys//:::::::::::::::/::dMMMMMMM\n");
+printf("Mmsssssssssssyyyhhddd+::::::::::-::::::::::::::+ooooooossssssssssssyyyyys//:::::::::::::::/:/NMMMMMM\n");
+printf("MMdsssssssssssyyhhhddh/::::::::::::::::::::::/oooooossssssssssssyyyyyyyyyo//:::::::::::::://:sMMMMMM\n");
+printf("MMMmsssssssssssyyhhhddh+:::::::::::::::::///+ooooossssssssssssyyyyyyyyyyyy+//::::::::::::::/:/MMMMMM\n");
+printf("MMMMNyssssssssssyyhhhddh+/::::::::::///////+oooosssssssssssssyyyyyyyyyyyyyo///:::::::::::::/::NMMMMM\n");
+printf("MMMMMNhssssssssssyyhhhddho////////////////+ooosssssssssssssyyyyyyyyyyyyyyys////:::::::::::://:hMMMMM\n");
+printf("MMMMMMMmysssssssssyyhhhddds//////////////+ooosssssssssssssyyyyyyyyyyyyyyyys////:::::::::::://:hNNMMM\n");
+printf("MMMMMMMMNdsssssssssyyhhddddho///////////+ooosssssssssssssyyyyyyyyyyyyyyyyys/////:::::::::://::dNNMMM\n");
+printf("MMMMMMMMMMmhsssssssyyhhhhddddho/://////++oosssssssssssssyyyyyyyyyyyyyyyyyyo//////:::::::///::/NNNMMM\n");
+printf("MMMMMMMMMMMMmhssssssyyhhhhhhhhmmyo/::///+ossssssssssssssyyyyyyyyyyyyyyyyys/////////::::///::/hMNNMMM\n");
+printf("MMMMMMMMMMMMMMmhysssyyyhhhhyhmNNNNmyo/::+ssssssssssssssyyyyyyyyyyyyyyyyyydho////////////:::+dNMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMNmdyyyyyyyhdNNNNNNNNNmdyssssssssssssssyyyyyyyyyyyyyyyyyymNNNdhs++///////+ohNMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMNNmmmmNNNNNNNNNNNNNNNNhsssssssssssyyyyyyyyyyyyyyyyyhmNNNNNNNNmddhhddmNNNNMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMNNNNNNNNhysssssssyyyyyyyyyyyyyyyyyhmNNNNNNNNNNNNNNNNNNNNMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmhyyyyyyyyyyyyyyyyyyyyhdmNNNNNNNNNNNNNNNNNNNMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMNmdhhyyyyyyyyyyhhddmNNNNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMM\n");
+printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNmmmmmNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+printf("                                                                                                        Smash Ultimate Hell :)\n");
+
+
+scanf("%i", &kirby);
+ 
+ system("pause");
+ return 0;
+}
